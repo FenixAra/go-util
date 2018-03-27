11490f4c1f8c5b62af045b706a732e5029af0caa
Fenix <aravind_dhan@yahoo.com>
2018-03-27 18:45:38 +0530

    Added log level and file display size

--- log/log.go
+++ log/log.go
@@ -10,19 +10,34 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
+const (
+	DEBUG = iota
+	INFO
+	WARN
+	ERROR
+	FATAL
+
+	SHORT = iota
+	FULL
+)
+
 type Logger struct {
-	l   *log.Logger
-	ref string
+	l        *log.Logger
+	level    int
+	fileSize int
+	ref      string
 }
 
-func New(ref string) *Logger {
+func New(ref string, level, fileSize int) *Logger {
 	l := &Logger{}
-	l.Init(ref)
+	l.Init(ref, level, fileSize)
 	return l
 }
 
-func (l *Logger) Init(ref string) error {
+func (l *Logger) Init(ref string, level, fileSize int) error {
 	l.ref = ref
+	l.level = level
+	l.fileSize = fileSize
 	if l.ref == "" {
 		refuuid, err := uuid.NewV4()
 		if err != nil {
@@ -40,33 +55,93 @@ func (l *Logger) GetRef() string {
 	return l.ref
 }
 
-func (l *Logger) Warn(a ...interface{}) {
-	l.l.Println(formatLog("WARN", a...)...)
+func (l *Logger) Debug(v ...interface{}) {
+	if l.level > DEBUG {
+		return
+	}
+
+	l.l.Println(l.formatLog("DEBUG", v...)...)
 }
 
-func (l *Logger) Error(a ...interface{}) {
-	l.l.Println(formatLog("ERROR", a...)...)
+func (l *Logger) Info(v ...interface{}) {
+	if l.level > INFO {
+		return
+	}
+
+	l.l.Println(l.formatLog("INFO", v...)...)
 }
 
-func (l *Logger) Info(a ...interface{}) {
-	l.l.Println(formatLog("INFO", a...)...)
+func (l *Logger) Warn(v ...interface{}) {
+	if l.level > WARN {
+		return
+	}
+
+	l.l.Println(l.formatLog("WARN", v...)...)
 }
 
-func (l *Logger) Debug(a ...interface{}) {
-	l.l.Println(formatLog("DEBUG", a...)...)
+func (l *Logger) Error(v ...interface{}) {
+	if l.level > ERROR {
+		return
+	}
+
+	l.l.Println(l.formatLog("ERROR", v...)...)
 }
 
-func (l *Logger) Fatal(a ...interface{}) {
-	l.l.Println(formatLog("FATAL", a...)...)
+func (l *Logger) Fatal(v ...interface{}) {
+	if l.level > FATAL {
+		return
+	}
+
+	l.l.Println(l.formatLog("FATAL", v...)...)
+}
+
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	if l.level > DEBUG {
+		return
+	}
+
+	l.l.Printf(l.formatLogf("DEBUG", format, v...))
+}
+
+func (l *Logger) Infof(format string, v ...interface{}) {
+	if l.level > INFO {
+		return
+	}
+
+	l.l.Printf(l.formatLogf("INFO", format, v...))
+}
+
+func (l *Logger) Warnf(format string, v ...interface{}) {
+	if l.level > WARN {
+		return
+	}
+
+	l.l.Printf(l.formatLogf("WARN", format, v...))
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	if l.level > ERROR {
+		return
+	}
+
+	l.l.Printf(l.formatLogf("ERROR", format, v...))
+}
+
+func (l *Logger) Fatalf(format string, v ...interface{}) {
+	if l.level > FATAL {
+		return
+	}
+
+	l.l.Printf(l.formatLogf("FATAL", format, v...))
 }
 
 // Format the log to contain the log levels
-func formatLog(logType string, a ...interface{}) []interface{} {
+func (l *Logger) formatLog(logType string, v ...interface{}) []interface{} {
 	var n []interface{}
 	n = append(n, "["+logType+"] ")
 	_, file, line, _ := runtime.Caller(2)
 	// If you want the short path not the full file path, you can uncomment everything below
-	/*
+	if l.fileSize == SHORT {
 		short := file
 		for i := len(file) - 1; i > 0; i-- {
 			if file[i] == '/' {
@@ -75,9 +150,35 @@ func formatLog(logType string, a ...interface{}) []interface{} {
 			}
 		}
 		file = short
-	*/
+	}
 
 	n = append(n, file+":"+strconv.Itoa(line)+":")
-	n = append(n, a...)
+	n = append(n, v...)
 	return n
+}
+
+// Format the log to contain the log levels
+func (l *Logger) formatLogf(logType string, format string, v ...interface{}) (string, []interface{}) {
+	var n []interface{}
+	prefix := "[%s] "
+	n = append(n, logType)
+	_, file, line, _ := runtime.Caller(2)
+	// If you want the short path not the full file path, you can uncomment everything below
+	if l.fileSize == SHORT {
+		short := file
+		for i := len(file) - 1; i > 0; i-- {
+			if file[i] == '/' {
+				short = file[i+1:]
+				break
+			}
+		}
+		file = short
+	}
+
+	prefix += "%s:%d: "
+	format = prefix + format
+	n = append(n, file)
+	n = append(n, line)
+	n = append(n, v...)
+	return format, n
 }
