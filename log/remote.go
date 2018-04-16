4c1a50c90043b57894d28b31df162f3d13352450
Fenix <aravind_dhan@yahoo.com>
2018-04-16 17:52:45 +0530

    Added API request and response logging

--- log/remote.go
+++ log/remote.go
@@ -10,28 +10,40 @@ import (
 )
 
 type Log struct {
-	Level     string    `json:"level"`
-	Timestamp time.Time `json:"timestamp"`
-	Title     string    `json:"message"`
-	Message   string    `json:"full_message"`
-	AppName   string    `json:"app_name"`
-	RefID     string    `json:"ref_id"`
-	File      string    `json:"file"`
-	Line      string    `json:"line"`
+	Level        string    `json:"level"`
+	Timestamp    time.Time `json:"timestamp"`
+	Title        string    `json:"message"`
+	Message      string    `json:"full_message"`
+	AppName      string    `json:"app_name"`
+	RefID        string    `json:"ref_id"`
+	File         string    `json:"file"`
+	Line         string    `json:"line"`
+	ResponseTime float64   `json:"response_time"`
+	StatusCode   int       `json:"status_code"`
+	Method       string    `json:"method"`
+	Request      string    `json:"request"`
 }
 
-func (l *Logger) PostToRemote(level, msg string) {
+func (l *Logger) LogAPIInfo(request, method string, responseTime float64, status int) {
+	l.postToRemote("INFO", "API Request info", request, method, responseTime, status)
+}
+
+func (l *Logger) postToRemote(level, msg, req, method string, responseTime float64, status int) {
 	client := &http.Client{}
 	file, line := l.GetFileLine(3)
 	reqData, err := json.Marshal(&Log{
-		Level:     level,
-		Timestamp: time.Now().UTC(),
-		Title:     msg,
-		Message:   msg,
-		AppName:   l.config.AppName,
-		RefID:     l.config.Reference,
-		File:      file,
-		Line:      strconv.Itoa(line),
+		Level:        level,
+		Timestamp:    time.Now().UTC(),
+		Title:        msg,
+		Message:      msg,
+		AppName:      l.config.AppName,
+		RefID:        l.config.Reference,
+		File:         file,
+		Line:         strconv.Itoa(line),
+		ResponseTime: responseTime,
+		StatusCode:   status,
+		Method:       method,
+		Request:      req,
 	})
 	if err != nil {
 		log.Println("Unable to marshal log request. Err:", err)
@@ -59,4 +71,8 @@ func (l *Logger) PostToRemote(level, msg string) {
 	}
 
 	return
+}
+
+func (l *Logger) PostToRemote(level, msg string) {
+	l.postToRemote(level, msg, "", "", 0, 0)
 }
