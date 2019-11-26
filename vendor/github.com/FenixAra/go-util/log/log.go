ba373794a8404e8b665afde9f604c79088f4dfc5
Rakesh <rakesh.c@lynk.co.in>
2019-11-26 15:52:53 +0530

    with newer version of uuid handled

---
+++ vendor/github.com/FenixAra/go-util/log/log.go
@@ -0,0 +1,230 @@
+package log
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+	"time"
+)
+
+type Level int
+
+const (
+	DEBUG Level = iota + 1
+	INFO
+	WARN
+	ERROR
+	FATAL
+
+	Debug = "Debug"
+	Info  = "Info"
+	Warn  = "Warn"
+	Error = "Error"
+	Fatal = "Fatal"
+
+	SHORT = iota
+	FULL
+
+	FilePathShort = "Short"
+	FilePathFull  = "Full"
+)
+
+type Logger struct {
+	l      *log.Logger
+	config *Config
+}
+
+func New(config *Config) *Logger {
+	l := &Logger{}
+	l.config = config
+	l.Init()
+	return l
+}
+
+func (l *Logger) Init() error {
+	l.l = log.New(os.Stdout, fmt.Sprintf("%v [%s] [ %s ] ", time.Now().UTC(), l.config.AppName, l.config.Reference), 0)
+	return nil
+}
+
+func (l *Logger) GetRef() string {
+	return l.config.Reference
+}
+
+func (l *Logger) Debug(v ...interface{}) {
+	if l.config.Level > DEBUG {
+		return
+	}
+
+	if l.config.remoteLogger {
+		go l.PostToRemote("DEBUG", fmt.Sprintf("%v [%s] [ %s ] ", time.Now().UTC(), l.config.AppName, l.config.Reference)+fmt.Sprintln(l.formatLog("DEBUG", v...)...))
+		return
+	}
+
+	l.l.Println(l.formatLog("DEBUG", v...)...)
+}
+
+func (l *Logger) Info(v ...interface{}) {
+	if l.config.Level > INFO {
+		return
+	}
+
+	if l.config.remoteLogger {
+		go l.PostToRemote("INFO", fmt.Sprintf("%v [%s] [ %s ] ", time.Now().UTC(), l.config.AppName, l.config.Reference)+fmt.Sprintln(l.formatLog("INFO", v...)...))
+		return
+	}
+
+	l.l.Println(l.formatLog("INFO", v...)...)
+}
+
+func (l *Logger) Warn(v ...interface{}) {
+	if l.config.Level > WARN {
+		return
+	}
+
+	if l.config.remoteLogger {
+		go l.PostToRemote("WARN", fmt.Sprintf("%v [%s] [ %s ] ", time.Now().UTC(), l.config.AppName, l.config.Reference)+fmt.Sprintln(l.formatLog("WARN", v...)...))
+		return
+	}
+
+	l.l.Println(l.formatLog("WARN", v...)...)
+}
+
+func (l *Logger) Error(v ...interface{}) {
+	if l.config.Level > ERROR {
+		return
+	}
+
+	if l.config.remoteLogger {
+		go l.PostToRemote("ERROR", fmt.Sprintf("%v [%s] [ %s ] ", time.Now().UTC(), l.config.AppName, l.config.Reference)+fmt.Sprintln(l.formatLog("ERROR", v...)...))
+		return
+	}
+
+	l.l.Println(l.formatLog("ERROR", v...)...)
+}
+
+func (l *Logger) Fatal(v ...interface{}) {
+	if l.config.Level > FATAL {
+		return
+	}
+
+	if l.config.remoteLogger {
+		go l.PostToRemote("FATAL", fmt.Sprintf("%v [%s] [ %s ] ", time.Now().UTC(), l.config.AppName, l.config.Reference)+fmt.Sprintln(l.formatLog("FATAL", v...)...))
+		return
+	}
+
+	l.l.Println(l.formatLog("FATAL", v...)...)
+}
+
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	if l.config.Level > DEBUG {
+		return
+	}
+
+	format, v = l.formatLogf("DEBUG", format, v...)
+	if l.config.remoteLogger {
+		l.PostToRemote("DEBUG", fmt.Sprintf("%v [%s] [ %s ] ", time.Now().UTC(), l.config.AppName, l.config.Reference)+fmt.Sprintf(format, v...))
+		return
+	}
+
+	l.l.Printf(format, v...)
+}
+
+func (l *Logger) Infof(format string, v ...interface{}) {
+	if l.config.Level > INFO {
+		return
+	}
+
+	format, v = l.formatLogf("INFO", format, v...)
+	if l.config.remoteLogger {
+		go l.PostToRemote("INFO", fmt.Sprintf("%v [%s] [ %s ] ", time.Now().UTC(), l.config.AppName, l.config.Reference)+fmt.Sprintf(format, v...))
+		return
+	}
+
+	l.l.Printf(format, v...)
+}
+
+func (l *Logger) Warnf(format string, v ...interface{}) {
+	if l.config.Level > WARN {
+		return
+	}
+
+	format, v = l.formatLogf("WARN", format, v...)
+	if l.config.remoteLogger {
+		go l.PostToRemote("WARN", fmt.Sprintf("%v [%s] [ %s ] ", time.Now().UTC(), l.config.AppName, l.config.Reference)+fmt.Sprintf(format, v...))
+		return
+	}
+
+	l.l.Printf(format, v...)
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	if l.config.Level > ERROR {
+		return
+	}
+
+	format, v = l.formatLogf("ERROR", format, v...)
+	if l.config.remoteLogger {
+		go l.PostToRemote("ERROR", fmt.Sprintf("%v [%s] [ %s ] ", time.Now().UTC(), l.config.AppName, l.config.Reference)+fmt.Sprintf(format, v...))
+		return
+	}
+
+	l.l.Printf(format, v...)
+}
+
+func (l *Logger) Fatalf(format string, v ...interface{}) {
+	if l.config.Level > FATAL {
+		return
+	}
+
+	format, v = l.formatLogf("FATAL", format, v...)
+	if l.config.remoteLogger {
+		go l.PostToRemote("FATAL", fmt.Sprintf("%v [%s] [ %s ] ", time.Now().UTC(), l.config.AppName, l.config.Reference)+fmt.Sprintf(format, v...))
+		return
+	}
+
+	l.l.Printf(format, v...)
+}
+
+// Format the log to contain the log levels
+func (l *Logger) formatLog(logType string, v ...interface{}) []interface{} {
+	var n []interface{}
+	n = append(n, "["+logType+"] ")
+	file, line := l.GetFileLine(3)
+
+	n = append(n, file+":"+strconv.Itoa(line)+":")
+	n = append(n, v...)
+	return n
+}
+
+// Format the log to contain the log levels
+func (l *Logger) formatLogf(logType string, format string, v ...interface{}) (string, []interface{}) {
+	var n []interface{}
+	prefix := "[%s] "
+	n = append(n, logType)
+	file, line := l.GetFileLine(3)
+
+	prefix += "%s:%d: "
+	format = prefix + format
+	n = append(n, file)
+	n = append(n, line)
+	n = append(n, v...)
+	return format, n
+}
+
+func (l *Logger) GetFileLine(n int) (string, int) {
+	_, file, line, _ := runtime.Caller(n)
+	// If you want the short path not the full file path, you can uncomment everything below
+	if l.config.FilePathSize == SHORT {
+		short := file
+		for i := len(file) - 1; i > 0; i-- {
+			if file[i] == '/' {
+				short = file[i+1:]
+				break
+			}
+		}
+		file = short
+	}
+	return file, line
+}
---
+++ vendor/github.com/FenixAra/go-util/log/remote.go
@@ -0,0 +1,128 @@
+package log
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/tomasen/realip"
+)
+
+const (
+	DependancyTypeHTTP  = "http"
+	DependancyTypeDB    = "db"
+	DependancyTypeRedis = "redis"
+)
+
+type Log struct {
+	Level          string    `json:"level"`
+	Timestamp      time.Time `json:"timestamp"`
+	Title          string    `json:"message"`
+	Message        string    `json:"full_message"`
+	AppName        string    `json:"app_name"`
+	RefID          string    `json:"ref_id"`
+	File           string    `json:"file"`
+	Line           string    `json:"line"`
+	ResponseTime   float64   `json:"response_time"`
+	StatusCode     int       `json:"status_code"`
+	Method         string    `json:"method"`
+	Request        string    `json:"request"`
+	UserAgent      string    `json:"user_agent"`
+	CustomerID     string    `json:"customer_id"`
+	IPAddress      string    `json:"ip_address"`
+	RequestGroup   string    `json:"request_group" example:"Ping"`
+	AppVersion     string    `json:"app_version" example:"App Version"`
+	TimeTaken      float64   `json:"time_taken" example:"1.11"`
+	DependancyType string    `json:"dependancy_type" example:"http,database"`
+	DependancyName string    `json:"dependancy_name" example:"googleapi,booktripsp"`
+}
+
+func (l *Logger) Log(lg *Log) {
+	lg.Level = Info
+	lg.Timestamp = time.Now().UTC()
+	lg.RefID = l.config.Reference
+	lg.AppName = l.config.AppName
+	file, line := l.GetFileLine(2)
+	lg.File = file
+	lg.Line = strconv.Itoa(line)
+	go l.sendLog(*lg)
+}
+
+func (l *Logger) LogAPIInfo(r *http.Request, responseTime float64, status int) {
+	if !l.config.remoteLogger {
+		return
+	}
+
+	file, line := l.GetFileLine(2)
+	l.postToRemote("INFO", "API Request info", file, r, responseTime, status, line)
+}
+
+func (l *Logger) postToRemote(level, msg, file string, r *http.Request, responseTime float64, status, line int) {
+	var method, req, ua, ip string
+	if r != nil {
+		method = r.Method
+		req = r.RequestURI
+		ua = r.UserAgent()
+		ip = realip.RealIP(r)
+	}
+	logData := &Log{
+		Level:        level,
+		Timestamp:    time.Now().UTC(),
+		Title:        msg,
+		Message:      msg,
+		AppName:      l.config.AppName,
+		RefID:        l.config.Reference,
+		File:         file,
+		Line:         strconv.Itoa(line),
+		ResponseTime: responseTime,
+		StatusCode:   status,
+		Method:       method,
+		Request:      req,
+		UserAgent:    ua,
+		IPAddress:    ip,
+	}
+
+	go l.sendLog(*logData)
+}
+
+func (l *Logger) sendLog(logData Log) {
+	client := &http.Client{}
+	reqData, err := json.Marshal(logData)
+	if err != nil {
+		log.Println("Unable to marshal log request. Err:", err)
+		return
+	}
+
+	request, err := http.NewRequest(http.MethodPost, l.config.RemoteLoggerURL, bytes.NewBuffer(reqData))
+	if err != nil {
+		log.Println("Unable to create new log request. Err:", err)
+		return
+	}
+
+	// Setting all headers
+	request.Header.Set("Content-Type", "application/json; charset=utf-8")
+	if l.config.RemoteToken != "" {
+		request.SetBasicAuth(l.config.RemoteUserName, l.config.RemoteToken)
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println("Unable to send log request. Err:", err)
+		return
+	}
+
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		log.Println("Status code is not 200 (OK). Got:", response.StatusCode)
+		// Handle the error codes
+		return
+	}
+}
+
+func (l *Logger) PostToRemote(level, msg string) {
+	file, line := l.GetFileLine(3)
+	l.postToRemote(level, msg, file, nil, 0, 0, line)
+}
